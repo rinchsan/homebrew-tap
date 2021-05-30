3a1df6761430bd5ba87f423977a980178796f4fa
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-30 18:53:12 +0900

    Brew formula update for gosimports version v0.0.18

--- Formula/gosimports.rb
+++ Formula/gosimports.rb
@@ -5,24 +5,24 @@
 class Gosimports < Formula
   desc "Command gosimports updates your Go import lines, adding missing ones, removing unreferenced ones, and removing redundant blank lines."
   homepage "https://pkg.go.dev/github.com/rinchsan/gosimports/cmd/gosimports"
-  version "0.0.17"
+  version "0.0.18"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.17/gosimports_0.0.17_darwin_amd64.tar.gz"
-    sha256 "2671ba3654d82d9cc6ec9c9ab782579b3fc0bc3f7eddd1649b36e1b741fda80a"
+    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_darwin_amd64.tar.gz"
+    sha256 "d85b434de122adb272a774e6d01086a3a1fd0f57879ce5043602d8363ad59665"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.17/gosimports_0.0.17_darwin_arm64.tar.gz"
-    sha256 "e5434f7555995d5e3447dfdf8613d458b18779395e1b59aafceefed653f2b469"
+    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_darwin_arm64.tar.gz"
+    sha256 "040696646b507422404467a4d2e6b000d4071df1109434dc0318d40e332750ea"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.17/gosimports_0.0.17_linux_amd64.tar.gz"
-    sha256 "ef5d93f0123a7414607a2e83395c0b79e9d54f2ad6d379cdbff24121c9b6636c"
+    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_linux_amd64.tar.gz"
+    sha256 "b909a35a3fafb8a1cdf3803da86f33e2094d81b2b0b14185a7df62bfed295338"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.17/gosimports_0.0.17_linux_arm64.tar.gz"
-    sha256 "bf5fd418906dd53e067f2fc7aac67a496b3ef6c4a030998f70d1d2e8c2f8948a"
+    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_linux_arm64.tar.gz"
+    sha256 "c5c6cbbbb3458f3bc3ff33a8d77746ccf975c18c30a9a84200a8bab5588bab6e"
   end
 
   def install
