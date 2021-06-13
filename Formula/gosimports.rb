645bee4e4d38e837718a632479905a1ef0b78a93
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-13 10:28:29 +0900

    Brew formula update for gosimports version v0.0.19

--- Formula/gosimports.rb
+++ Formula/gosimports.rb
@@ -5,24 +5,29 @@
 class Gosimports < Formula
   desc "Command gosimports updates your Go import lines, adding missing ones, removing unreferenced ones, and removing redundant blank lines."
   homepage "https://pkg.go.dev/github.com/rinchsan/gosimports/cmd/gosimports"
-  version "0.0.18"
+  version "0.0.19"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_darwin_amd64.tar.gz"
-    sha256 "d85b434de122adb272a774e6d01086a3a1fd0f57879ce5043602d8363ad59665"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.19/gosimports_0.0.19_darwin_amd64.tar.gz"
+      sha256 "2812b5271de6f885b81f7d62564615a8d24403ad45373df2bac93267e94c9d92"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.19/gosimports_0.0.19_darwin_arm64.tar.gz"
+      sha256 "469d359d14c98dd40d5c5dfc879330a65b53167f5d8669e947f4dbed2e7433a3"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_darwin_arm64.tar.gz"
-    sha256 "040696646b507422404467a4d2e6b000d4071df1109434dc0318d40e332750ea"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_linux_amd64.tar.gz"
-    sha256 "b909a35a3fafb8a1cdf3803da86f33e2094d81b2b0b14185a7df62bfed295338"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/rinchsan/gosimports/releases/download/v0.0.18/gosimports_0.0.18_linux_arm64.tar.gz"
-    sha256 "c5c6cbbbb3458f3bc3ff33a8d77746ccf975c18c30a9a84200a8bab5588bab6e"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.19/gosimports_0.0.19_linux_amd64.tar.gz"
+      sha256 "f86221d6e5c2a2bf8ab69a3cfa4ed68afc7856d02be466c8dcf6bdb10a502a11"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.19/gosimports_0.0.19_linux_arm64.tar.gz"
+      sha256 "45ea7920bc5f00280f65b1f251826e87a499019dcb6702e7f6df66822791b121"
+    end
   end
 
   def install
