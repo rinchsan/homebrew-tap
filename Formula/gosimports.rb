5aa665b2553d9f5d75d6c467699ac98c2a68d749
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-04 00:55:02 +0900

    Brew formula update for gosimports version v0.0.28

--- Formula/gosimports.rb
+++ Formula/gosimports.rb
@@ -5,28 +5,28 @@
 class Gosimports < Formula
   desc "Command gosimports updates your Go import lines, adding missing ones, removing unreferenced ones, and removing redundant blank lines."
   homepage "https://pkg.go.dev/github.com/rinchsan/gosimports/cmd/gosimports"
-  version "0.0.27"
+  version "0.0.28"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.27/gosimports_0.0.27_darwin_amd64.tar.gz"
-      sha256 "ce1ebd78d82a360adf90350538b19529f1328b2c9b560d078707e2a2f2fed35d"
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.28/gosimports_0.0.28_darwin_amd64.tar.gz"
+      sha256 "d850cae621ff42456a4c421e87462eebab76e9530c7ee945851d4811198548fb"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.27/gosimports_0.0.27_darwin_arm64.tar.gz"
-      sha256 "9c12e647eb4359710e737f76258e49977e961c0b73bcd83f0586f8942620233b"
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.28/gosimports_0.0.28_darwin_arm64.tar.gz"
+      sha256 "f758aae78c2ac31c0d7e3ddfd3ea236ba08b810913b258de8f815442d396da16"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.27/gosimports_0.0.27_linux_amd64.tar.gz"
-      sha256 "759628b99c1c9e9c0559b6f7d3d1613098b407e4f9f6b66d3b64cb7ea0bd3b81"
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.28/gosimports_0.0.28_linux_amd64.tar.gz"
+      sha256 "fd10d0df1e12114c11dae916ee74073e4e2fec500475c9dab077afda96db9f5d"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.27/gosimports_0.0.27_linux_arm64.tar.gz"
-      sha256 "3573b42cbebc3f53b94ff4ad3f91c04d5ae0016923fcaef25fcd16de89cc3ff8"
+      url "https://github.com/rinchsan/gosimports/releases/download/v0.0.28/gosimports_0.0.28_linux_arm64.tar.gz"
+      sha256 "1a8625906cfe1cf043a4820d384f33039c38254e325e9df547d325e950f1ccb8"
     end
   end
 
